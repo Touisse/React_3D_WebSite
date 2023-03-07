2c09c6d6cc7fe65dd97318f4070010aa05f67032
Touisse <yassin.touisd@gmail.com>
2023-03-07 14:08:54 +0100

    the works section and contact : done

--- 3d_Website/src/Components/Contact.jsx
+++ 3d_Website/src/Components/Contact.jsx
@@ -1,12 +1,89 @@
 import React from "react";
 import styled from "styled-components";
+import Map from "./Map";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  @media only screen and (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+  }
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #e8e6e6;
+`;
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`;
+const Right = styled.div`
+  flex: 1;
 `;
 const Contact = () => {
-  return <Section>Contact</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name" />
+            <Input placeholder="Email" />
+            <TextArea placeholder="Write your Message" rows={10} />
+            <Button>Send</Button>
+          </Form>
+        </Left>
+        <Right>
+          <Map />
+        </Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Contact;
--- 3d_Website/src/Components/Works.jsx
+++ 3d_Website/src/Components/Works.jsx
@@ -1,13 +1,86 @@
 import React from "react";
 import styled from "styled-components";
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "UI/UX",
+  "Data Science",
+];
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  display: flex;
+  width: 1400px;
+  justify-content: space-between;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const ListItems = styled.li`
+  font-size: 90px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+  ::after {
+    content: "${(props) => props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    width: 0px;
+    color: pink;
+    white-space: nowrap;
+  }
+  &:hover {
+    ::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to {
+          width: 100%;
+        }
+      }
+    }
+  }
 `;
 
 const Works = () => {
-  return <Section>Works</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItems key={item} text={item}>
+                {item}
+              </ListItems>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Works;
